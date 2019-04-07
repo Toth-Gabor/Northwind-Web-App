c2a3babbcb91fb071346abbc30d6134443f45efd
Toth-Gabor <upcgabor@gmail.com>
2019-04-07 09:45:38 +0200

    Task3Result class created

---
+++ src/main/java/com/codecool/web/model/Task3Result.java
@@ -0,0 +1,14 @@
+package com.codecool.web.model;
+
+public class Task3Result {
+
+    private String company;
+    
+    public Task3Result(String company) {
+        this.company = company;
+    }
+    
+    public String getCompany() {
+        return company;
+    }
+}
