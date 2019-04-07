c293eabe4f7c2911c75240bede27034dba9659bd
Toth-Gabor <upcgabor@gmail.com>
2019-04-07 11:14:40 +0200

    Task4Service done

---
+++ src/main/java/com/codecool/web/service/simple/SimpleTask4Service.java
@@ -0,0 +1,22 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.Task4Dao;
+import com.codecool.web.model.Task4Result;
+import com.codecool.web.service.Task4Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class SimpleTask4Service implements Task4Service {
+    
+    private final Task4Dao task4Dao;
+    
+    public SimpleTask4Service(Task4Dao task4Dao) {
+        this.task4Dao = task4Dao;
+    }
+    
+    @Override
+    public List<Task4Result> getResults() throws SQLException {
+        return task4Dao.findAll();
+    }
+}
